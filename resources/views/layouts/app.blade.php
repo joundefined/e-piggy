615dbacf801a803829814f22af3cf248c2b16763
dorin-palanciuc <dorin.palanciuc@gmail.com>
2020-10-10 15:23:13 +0200

    minor style changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                   <div><img src="img/logo-transparent.png" alt="weee!" style="height : 40px"/></div>
+                   <div><img src="img/logo-transparent.png" alt="weee!" style="height : 40px" class="pr-3"/></div>
                    <div>e-Piggy</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
